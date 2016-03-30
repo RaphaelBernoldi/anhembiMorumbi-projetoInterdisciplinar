3ea0ddb6c838cb785c40b813c1eebc0fa0a76d80
Raphael Bernoldi <raphaelbernoldi@gmil.com>
2016-03-29 21:30:16 -0300

    Criacao do projeto / teste de ambiente

---
+++ sim-camp/src/main/java/br/com/anhembi/simcamp/entity/Teste.java
@@ -0,0 +1,12 @@
+package br.com.anhembi.simcamp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Teste  {
+	
+	@Id
+	private Long ig;
+
+}
---
+++ sim-camp/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Sim-Camp está no ar!</h2>
+</body>
+</html>
